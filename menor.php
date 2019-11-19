e2a445caab4e55ed8d701dcf809676ad7336c80c
Bianca <bpereiraalmeida@gmail.com>
2019-11-19 12:17:33 -0300

    menor Inteiro

---
+++ menor.php
@@ -0,0 +1,19 @@
+<?php 
+    function menorInteiro($num1, $num2, $num3) 
+    { 
+    $i = 1;
+    while ($i >= 1) {
+        if($i % $num1 == 0 && $i % $num2 == 0 && $i % $num3 == 0){
+            $i = $i+1;
+            break;
+        }
+    }
+    return $i;
+    } 
+
+    $num1 = 2;
+    $num2 = 3;
+    $num3 = 10;
+
+    echo menorInteiro($num1, $num2, $num3);
+?>
